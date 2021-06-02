646d170f6241fd218cfb2548195bfc4f2761bd0d
Dimitri Imfeld <dimfeld@protonmail.ch>
2021-06-02 14:38:49 +0200

    Simple table layout for clients

--- app/src/main/java/ch/leytto/cynoclient/ui/client/ClientFragment.kt
+++ app/src/main/java/ch/leytto/cynoclient/ui/client/ClientFragment.kt
@@ -4,17 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import ch.leytto.cynoclient.ClientListAdapter
 import ch.leytto.cynoclient.CynoClientApplication
 import ch.leytto.cynoclient.R
 import ch.leytto.cynoclient.db.entities.Client
 import ch.leytto.cynoclient.viewmodels.ClientViewModel
-import ch.leytto.cynoclient.viewmodels.DogViewModel
 import ch.leytto.cynoclient.viewmodels.ViewModelFactory
 
 class ClientFragment : Fragment() {
@@ -26,13 +24,18 @@ class ClientFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_clients_list, container, false)
 
-        val recyclerView = root.findViewById<RecyclerView>(R.id.client_recyclerview)
-        val adapter = ClientListAdapter()
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(context)
+        val clientsTable = root.findViewById<TableLayout>(R.id.clients_table)
 
         clientViewModel.AllClients.observe(viewLifecycleOwner) { clients: List<Client> ->
-            clients.let { adapter.submitList(it) }
+            clients.forEach {
+                val row = LayoutInflater.from(context).inflate(R.layout.clients_list_row, null)
+
+                row.findViewById<TextView>(R.id.first_name).text = it.firstname
+                row.findViewById<TextView>(R.id.last_name).text = it.lastname
+                row.findViewById<TextView>(R.id.contact_info).text = it.email
+
+                clientsTable.addView(row);
+            }
         }
 
         return root
