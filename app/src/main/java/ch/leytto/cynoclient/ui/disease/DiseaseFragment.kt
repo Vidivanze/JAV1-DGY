77064bb52f7bdc059a7f808a2f2ea040026d4a09
PEREIRA Gabriel <Gabriel.PEREIRA@cpnv.ch>
2021-06-18 14:26:12 +0200

    Refactor diseases list

--- app/src/main/java/ch/leytto/cynoclient/ui/disease/DiseaseFragment.kt
+++ app/src/main/java/ch/leytto/cynoclient/ui/disease/DiseaseFragment.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import ch.leytto.cynoclient.CynoClientApplication
-import ch.leytto.cynoclient.DiseaseListAdapter
 import ch.leytto.cynoclient.R
 import ch.leytto.cynoclient.viewmodels.DiseaseViewModel
 import ch.leytto.cynoclient.viewmodels.ViewModelFactory
@@ -24,13 +23,16 @@ class DiseaseFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_diseases_list, container, false)
 
-        val recyclerView = root.findViewById<RecyclerView>(R.id.disease_recyclerview)
-        val adapter = DiseaseListAdapter()
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(context)
+        val diseasesTable = root.findViewById<TableLayout>(R.id.diseases_table)
 
         diseaseViewModel.AllDiseases.observe(viewLifecycleOwner) { diseases ->
-            diseases.let { adapter.submitList(it) }
+            diseases.forEach {
+                val row = LayoutInflater.from(context).inflate(R.layout.diseases_list_row, null)
+
+                row.findViewById<TextView>(R.id.noun).text = it.noun
+
+                diseasesTable.addView(row);
+            }
         }
 
         return root
